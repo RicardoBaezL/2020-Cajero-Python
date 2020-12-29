85ad535f6b554d6bb2f39a0a2e34a1215a3584d5
Ricky <ricardombaezleal@gmail.com>
2020-12-29 00:44:25 -0300

    Subido a master

--- cajero.py
+++ cajero.py
@@ -1,23 +1,24 @@
 from random import randint
-
+import cuentaCliente
 class Cajero():
     stado=False
-    saldo=10000
+    saldoPesos=cuentaCliente.cuentaCliente.saldoPesos
     
     def setEstado(self):
-        self.stado=True
-    
+        self.stado=True    
     def getEstado(self):
         if(self.stado):
             return "El cajero esta activo"
         else:
             return "El cajero no esta activo"
-
     def sacoSaldoCuenta(self,retiro):
-       self.saldo=self.saldo-retiro
-       
+        if(retiro<elCajero.saldoPesos):
+            elCajero.saldoPesos=elCajero.saldoPesos-retiro
+            return print("Su saldo es: $",elCajero.saldoPesos, " Usted reitor: $",retiro)
+        else:
+            return print("No tiene saldo suficiente")      
     def pongoSaldoCuenta(self,deposito):
-       self.saldo=self.saldo+deposito
+       elCajero.saldoPesos=elCajero.saldoPesos+deposito
     
     def generoNuevaClaveHomeBanking(self):
         numeros_aleatorios = [randint(1,20) for i in range(20)]
@@ -28,21 +29,17 @@ elCajero=Cajero()
 elCajero.setEstado()
 print(elCajero.getEstado())
 
-opcion=4
+opcion=2
 
 if(opcion==1):
-    print("Su saldo es: ",elCajero.saldo)
+    print("Su saldo es: ",elCajero.saldoPesos)
 if(opcion==2):
     retiro=100
     #retiro=10000000
-    elCajero.sacoSaldoCuenta(retiro)
-    if(retiro<elCajero.saldo):
-        print("Su saldo es: $",elCajero.saldo, " Usted reitor: $",retiro)
-    else:
-        print("No tiene saldo suficiente")  
+    elCajero.sacoSaldoCuenta(retiro) 
 if(opcion==3):
     depositoPlata=1000
     elCajero.pongoSaldoCuenta(depositoPlata)
-    print(elCajero.saldo)
+    print(elCajero.saldoPesos)
 if(opcion==4):
     print("Su clave es: ",elCajero.generoNuevaClaveHomeBanking())
---
+++ cuentaCliente.py
@@ -0,0 +1,4 @@
+class cuentaCliente():
+    saldoPesos=100000
+    saldoDolaes=2000
+    Nombre="Ricardo"
